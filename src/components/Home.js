29df9934e5e78db667eda560c94ffc4acfc4f250
Juan F Contreras <jufcontrerasco@unal.edu.co>
2019-05-25 16:38:10 -0500

    First Commit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+
+class Home extends React.Component{
+
+    render(){
+        return(
+            <h1>Hola</h1>
+            )
+    }
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Router>
+      <Route path="/" component={App}></Route>
+    </Router>,
+  document.body.appendChild(document.createElement('app'))
+  )
+})
