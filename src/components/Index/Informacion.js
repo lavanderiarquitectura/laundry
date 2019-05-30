665cec2e41ee1d6ba5637860beca2a771d0116c3
Juan F Contreras <jufcontrerasco@unal.edu.co>
2019-05-30 00:09:35 -0500

    Change in views(Login and Register), Add view Home

--- src/components/Index/Informacion.js
+++ src/components/Index/Informacion.js
@@ -4,12 +4,29 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import logo from '../../img/logo.png';
 
+import InputAdornment from '@material-ui/core/InputAdornment';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MeetingRoom from '@material-ui/icons/MeetingRoom';
+import ShoppingCart from '@material-ui/icons/ShoppingCart';
+
+
 class Informacion extends React.Component{
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          name: 'Juan Felipe Contreras',
+          room: '401',
+          cloths: '2'
+        };
+      //  this.changeState = this.changeState.bind(this);
+    }
     
     render(){
 
         const styles ={
-            divLogin :{
+            div :{
                 padding: "15px 15px 8px 15px",
                 borderRadius: "10px",
                 marginTop: "20px",
@@ -19,9 +36,9 @@ class Informacion extends React.Component{
             },
             botonInicio:{
                 width: '90%',
-                height: '40px',
-                marginBottom: '5px',
-                marginTop:'8px',
+                height: '45px',
+                marginBottom: '0px',
+                marginTop:'5px',
             },
             label:{
                 color: 'white',
@@ -36,7 +53,8 @@ class Informacion extends React.Component{
                 marginBottom: '0',
             },
             logo:{
-                width: '60%',
+                width: '100%',
+                marginLeft:'5px',
             },
             olvidaste:{
                 fontSize:"12px",
@@ -44,42 +62,121 @@ class Informacion extends React.Component{
                 marginTop:"5px",
                 textAlign: "center",
             },
+            divBottom:{
+                marginTop:"5px",
+            },
+            text:{
+                marginBottom:"3px",
+            },
+            textR:{
+                marginBottom:"3px",
+                width: "46%"
+            },
+            textC:{
+                marginBottom:"3px",
+                marginLeft: "5px",
+                width: "50%"
+            },
+            information:{
+                paddingTop:"16px",
+                paddingRight:"0",
+            },
+            buttons:{
+                paddingTop:"10px",
+            },
+            divInfo:{
+                paddingLeft:"5px",
+            },
+            row:{
+                padding: "0",
+                marginLeft: "5px",
+                marginRight: "5px",
+                backgroundColor: "white",
+                paddingBottom: "5px",
+            },
+            rowBottom:{
+                padding: "0",
+                marginLeft: "5px",
+                marginRight: "5px",
+                marginTop:"10px",
+                backgroundColor: "white",
+                paddingBottom: "5px",
+            },
+            welcome:{
+                textAlign:"center",
+                marginTop:"10px",
+            },
 
         }
 
         return(
-            <div className="container col-3" style={styles.divLogin}>
-                <form>
-                <div className="form-group" style={styles.divInput}>
-                    <img id="logo" src={logo} style={styles.logo}></img>
+  
+            <div className="container col-5" style={styles.div}>               
+                <div className='row' style={styles.row}>
+                    <div className='col-3'>
+                        <img id="logo" src={logo} style={styles.logo}></img>
+                    </div>
+                    <div className='col' style={styles.information}>
+                        <div style={styles.divInfo}>                        
+                        <TextField
+                            disabled
+                            id="input-with-icon-textfield"
+                            label="Name User"
+                            style={styles.text}
+                            value={this.state.name}
+                            InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                <AccountCircle />
+                                </InputAdornment>
+                            ),
+                            }}
+                        />
+                        <TextField
+                            disabled
+                            id="input-with-icon-textfield"
+                            label="Number Room"
+                            style={styles.textR}
+                            value={this.state.room}
+                            InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                <MeetingRoom />
+                                </InputAdornment>
+                            ),
+                            }}
+                        />
+                        <TextField
+                            disabled
+                            id="input-with-icon-textfield"
+                            label="Number cloths"
+                            style={styles.textC}
+                            value={this.state.cloths}
+                            InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                <ShoppingCart/>
+                                </InputAdornment>
+                            ),
+                            }}
+                        />
+                        </div>                        
+                    </div>
+                    <div className='col-4' style={styles.buttons}>
+                        <div className="" style={styles.divInput}>                
+                        <Link to='#'><Button onClick="" variant="outlined" focusVisible style={styles.botonInicio} color="primary">Mis Prendas</Button></Link>
+                        </div>
+                        <div className="" style={styles.divInput}>                
+                        <Link to='#'><Button onClick="" variant="outlined" focusVisible style={styles.botonInicio} color="primary">Factura</Button></Link>
+                        </div>
+                    </div>
                 </div>
-                <div className="form-group" style={styles.divInput}>
-                    <TextField
-                        autoFocus
-                        id="cedula"
-                        label="Número de Cedula"
-                        margin="normal"
-                        style = {styles.inputs}
-                        variant="outlined"
-                    />
-               </div>
-                <div className="form-group" style={styles.divInput}>                
-                <TextField
-                        id="password"
-                        label="Contraseña"
-                        type="password"
-                        style = {styles.inputs}
-                        margin="normal"
-                        variant="outlined"
-                    />
-               </div>
-                <div className="form-group" style={styles.divInput}>                
-                    <Link to='/'><Button onClick={this.props.onChange} variant="outlined" focusVisible style={styles.botonInicio} color="primary">Ingresar</Button></Link>
+                <div className='row' style={styles.rowBottom}>
+                    <div className='col-12'>
+                    <h1 style={styles.welcome}>Welcome!</h1>
+                    </div>
                 </div>
-                <div className="form-group" style={styles.olvidaste}><a>¿Olvidaste tu </a><Link to='/'><a>contraseña</a></Link><a>?</a></div>
-               </form>
-                
-            </div>
+            </div>            
             )
     }
 }
