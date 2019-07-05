705adf61233acc2defec2bb428d2820d965fd094
Juan F Contreras <jufcontrerasco@unal.edu.co>
2019-07-05 01:28:17 -0500

    Interoperabilidad finalizada

---
+++ src/components/Auxiliares/Error.js
@@ -0,0 +1,20 @@
+import {Link} from 'react-router-dom';
+import React, {Component} from 'react';
+
+class Error extends React.Component{
+
+    render(){
+
+        var error = 
+
+        <div style={{textAlign: "center", margin: "10px 0 0 0"}}><h1>ERROR 403 Forbidden</h1><h1>You don't have permission to access this route.</h1></div>
+
+        return(
+            <div>
+                {error}
+            </div>
+            )
+    }
+}
+
+export default Error;
